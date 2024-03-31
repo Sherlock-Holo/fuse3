1628fc6032d8eb394655087920799c3a04095f52
owl <ouyangjun1999@gmail.com>
2024-03-31 17:03:34 +0800

    feat: fix code

--- src/helper.rs
+++ src/helper.rs
@@ -22,6 +22,19 @@ pub fn get_first_null_position(data: impl AsRef<[u8]>) -> Option<usize> {
     data.as_ref().iter().position(|char| *char == 0)
 }
 
+// Some platforms like Linux x86_64 have mode_t = u32, and lint warns of a trivial_numeric_casts.
+// But others like macOS x86_64 have mode_t = u16, requiring a typecast. So, just silence lint.
+#[cfg(target_os = "linux")]
+#[allow(trivial_numeric_casts)]
+/// returns the mode for a given file kind and permission
+pub const fn mode_from_kind_and_perm(kind: FileType, perm: u16) -> u32 {
+    kind.const_into_mode_t() | perm as mode_t
+}
+
+// Some platforms like Linux x86_64 have mode_t = u32, and lint warns of a trivial_numeric_casts.
+// But others like macOS x86_64 have mode_t = u16, requiring a typecast. So, just silence lint.
+#[cfg(all(not(target_os = "linux"), any(target_os = "freebsd", target_os = "macos")))]
+#[allow(trivial_numeric_casts)]
 /// returns the mode for a given file kind and permission
 pub const fn mode_from_kind_and_perm(kind: FileType, perm: u16) -> u32 {
     (kind.const_into_mode_t() | perm as mode_t) as u32
